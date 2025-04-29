5646327b72bf4b490031e478179ca33d102683db
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-29 18:53:00 +0500

    Temirzhan the best

--- src/main/java/org/example/backdating.java
+++ src/main/java/org/example/backdating.java
@@ -1,4 +1,5 @@
 package org.example;
 
 public class backdating {
+writed smth for test smthng
 }
--- src/main/java/org/example/calsdasd.java
+++ src/main/java/org/example/calsdasd.java
@@ -1,4 +1,5 @@
 package org.example;
 
-public class calsdasd {
+public class calsdasd  {
+    Temirzhan the best
 }
